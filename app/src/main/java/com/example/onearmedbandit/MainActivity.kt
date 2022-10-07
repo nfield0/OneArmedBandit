70829c540c9217f4d180dfa80234c661866661f9
TheBogman02 <92158821+TheBogman02@users.noreply.github.com>
2022-10-07 21:53:54 +0100

    Added 2 new activities, styled buttons

--- app/src/main/java/com/example/onearmedbandit/MainActivity.kt
+++ app/src/main/java/com/example/onearmedbandit/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.onearmedbandit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private val onCreate = "OnCreate"
@@ -17,17 +19,26 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{ roll()}
+        val instructionsButton: Button = findViewById(R.id.instructions)
+        instructionsButton.setOnClickListener{
+            val intent = Intent(this, InstructionsActivity::class.java)
+            startActivity(intent)
+        }
     }
-
+    private var spinCount = 0
+    private var winCount = 0
+    private var winLossRatio = 0.0
     private fun roll(){
+        spinCount++
         val rolled = "RollCalled"
+
+
         val spinner = Spin(4)
         val spin1 = spinner.roll()
         val spin2 = spinner.roll()
         val spin3 = spinner.roll()
 
         Log.d(rolled, "Random Number 1: " + spin1 + "Random Number 2: " + spin2 + "Random Number 3: " + spin3)
-
 
         val spinImage1: ImageView = findViewById(R.id.imageView)
         val spinImage2: ImageView = findViewById(R.id.imageView2)
@@ -50,6 +61,7 @@ class MainActivity : AppCompatActivity() {
 
         if(spin1 == spin2 && spin2 == spin3)
         {
+            winCount++
             resultImage.setImageResource(R.drawable.winner)
         }
         else
@@ -57,7 +69,13 @@ class MainActivity : AppCompatActivity() {
             resultImage.setImageResource(R.drawable.badluck)
         }
 
-
+        val numSpinsView: TextView = findViewById(R.id.numSpins)
+        val numWinsView: TextView = findViewById(R.id.numWins)
+        var winLossRatioView: TextView = findViewById(R.id.winLossRatio)
+        numSpinsView.text = "$spinCount"
+        numWinsView.text = "$winCount"
+        winLossRatio = (winCount.toDouble() / spinCount).toDouble()
+        winLossRatioView.text =  String.format("%.4f", winLossRatio)
     }
 
     private fun draw(num: Int): Int {
@@ -78,6 +96,7 @@ class MainActivity : AppCompatActivity() {
 
 class Spin (private val numValues: Int)
 {
+
     fun roll(): Int {
         return (1..numValues).random()
     }
